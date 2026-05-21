926f282c7671fc4ce2bb8488573ac36365aa2564
Paulo Perdomo <paulo.perdomo@cobistopaz.com>
2026-05-21 09:45:49 -0500

    Init project setup

---
+++ src/main/java/com/inventory/order/test/OrderInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.order.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventory/order/test/OrderInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.order.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
